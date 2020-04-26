d53d8878947c9e65e8612b12f86242707fac972f
Daniel Machlab <daniel@hadlim.com>
2020-04-26 14:18:30 -0500

    Read sp500 values

--- project.R
+++ project.R
@@ -1,1 +1,7 @@
 library(quantmod)
+
+# get daily S&P500 index
+SPX <- getSymbols("^GSPC", auto.assign = FALSE, from = "1980-01-01")
+head(SPX)
+
+# calculate daily log returns of the adjusted closing prices
